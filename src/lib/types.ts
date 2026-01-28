5d7d1e2fc639807674fb529cc402b1318b3db368
tyree <tyree.fullstack@gmail.com>
2026-01-28 14:30:57 -0500

    fixing ingredients to include quantity & unit

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,17 @@
+import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 import {
   ingredientsTable,
   recipeIngredientsTable,
   recipesTable,
 } from "@/db/schema";
-import { createSelectSchema } from "drizzle-zod";
-import type { z } from "zod";
 
 const recipeSchema = createSelectSchema(recipesTable).partial({ id: true });
-const ingredientSchema = createSelectSchema(ingredientsTable).partial({
-  id: true,
-});
+const ingredientSchema = createSelectSchema(ingredientsTable)
+  .partial({
+    id: true,
+  })
+  .extend({ quantity: z.coerce.number().min(0), unit: z.string() });
 const recipeIngredientSchema = createSelectSchema(recipeIngredientsTable);
 
 export type Recipe = z.infer<typeof recipeSchema>;
