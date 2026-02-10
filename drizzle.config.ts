262a934ba570429544c3555d24f694661b8d900b
tyree <tyree.fullstack@gmail.com>
2026-02-10 18:31:43 -0500

    change to error handling

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,9 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 import { errors } from "@/lib/errors";
-import { logger } from "@/lib/logger";
 
 if (!process.env.DATABASE_URL) {
-  logger.fatal("database env variable not found");
-  throw errors.ENV_VAR_NOT_FOUND;
+  throw errors.envVarNotFound("DATABASE_URL");
 }
 
 export default defineConfig({
--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,4 +1,22 @@
+import { logger } from "@/lib/logger";
+
+const envVarNotFound = (envVar: string) => {
+  logger.fatal(`environment variables ${envVar} not found`);
+  return new Error(`unable to local environment variable ${envVar}`);
+};
+
+const contextNotFound = () => {
+  logger.fatal("useCartContext used outside of CartContextProvider");
+  return CONTEXT_NOT_FOUND;
+};
+
+const localStorageMalformed = () => {
+  logger.error("local storage does not contain the expected value");
+  return LOCAL_STORAGE_MALFORMED;
+};
+
 const ENV_VAR_NOT_FOUND = new Error("unable to locate environment variable");
+
 const CONTEXT_NOT_FOUND = new Error(
   "useCartContext must be used within CartContextProvider",
 );
@@ -7,6 +25,9 @@ const LOCAL_STORAGE_MALFORMED = new Error(
 );
 
 export const errors = {
+  envVarNotFound,
+  contextNotFound,
+  localStorageMalformed,
   ENV_VAR_NOT_FOUND,
   CONTEXT_NOT_FOUND,
   LOCAL_STORAGE_MALFORMED,
